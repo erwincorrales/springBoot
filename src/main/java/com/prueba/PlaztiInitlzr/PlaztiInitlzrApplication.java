89bd89867ee2c5c966b3a13ac140a7c99cd36642
Erwin <erwin.corrales@zabud.com.co>
2022-11-22 17:54:06 -0500

    springboot initializr

---
+++ src/main/java/com/prueba/PlaztiInitlzr/PlaztiInitlzrApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.PlaztiInitlzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaztiInitlzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaztiInitlzrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/PlaztiInitlzr/PlaztiInitlzrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.PlaztiInitlzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaztiInitlzrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
